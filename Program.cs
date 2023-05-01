3e8162064ed2e3f960418d328af23ea8b2b78c21
Rasmus <rasmus.hirvesoo@tktk.ee>
2023-05-01 13:25:26 +0300

    add week15 code

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿
+List<Movie> myMovies = new List<Movie>();
+string[] data = GetDataFromMyFile();
+//ReadDataFromArray(data);
+
+foreach(string line in data)
+{
+    string[] tempArray = line.Split(new char[] {';'}, StringSplitOptions.RemoveEmptyEntries);
+    Movie newMoview = new Movie(tempArray[0], tempArray[2]);
+    myMovies.Add(newMoview);
+}
+
+foreach(Movie movie in myMovies)
+{
+    Console.WriteLine($"One of my favorite movies {movie.Title} was released in {movie.Year}.");
+}
+
+
+
+static string[] GetDataFromMyFile()
+{
+    string filePath = @"C:\Users\nxt06\OneDrive\Desktop\movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach(string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+class Movie
+{
+    string title; //fields
+    string year;
+
+    public string Title
+    {
+        get { return title; }
+    }
+    public string Year
+    {
+        get { return year; }
+    }
+
+    public Movie(string _title, string _year)
+    {
+        title = _title;
+        year = _year;
+    }
+}
